5a4019848ac33429c7e216a9f53d500fe09fa260
ben <bend@smarti.co.il>
2021-12-22 06:10:25 +0200

    router after date validation

--- src/app/_shared/constants/validators.ts
+++ src/app/_shared/constants/validators.ts
@@ -1,10 +1,19 @@
 import * as moment from 'moment';
+import Diff = moment.unitOfTime.Diff;
 import { FormControl } from '@angular/forms';
 
-export const isDateGreaterOrEqual = (minControlName: FormControl, control: FormControl) => {
-  const minValue = moment(minControlName.value).utc();
-  const maxValue = moment(control.value).utc();
-  const greater = maxValue.diff(minValue, 'days') >= 0;
+export const isDateGreaterOrEqual = (args: { minControl: FormControl, unitOfTime?: Diff }, control: FormControl) => {
+  if (!args.unitOfTime) {
+    args.unitOfTime = 'days';
+  }
 
+  if (!args.minControl.value || !control.value) {
+    return null;
+  }
+
+  const minValue = moment(args.minControl.value).utc();
+  const maxValue = moment(control.value).utc();
+  const greater = maxValue.diff(minValue, args.unitOfTime) >= 0;
+  
   return !greater ? { range: true } : null;
 }
