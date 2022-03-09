a5da2ff0978109b21973c64d5a404639f31c862e
ben <bend@smarti.co.il>
2022-03-09 03:09:48 +0200

    done report production criteria columns

--- src/app/platform/reports/historical/results/columns-dialog/columns-dialog.component.ts
+++ src/app/platform/reports/historical/results/columns-dialog/columns-dialog.component.ts
@@ -1,19 +1,28 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-
-import { SelectItemModel } from 'src/app/_shared/models/select-item.model';
+import { Fade } from 'src/app/_shared/constants/animations';
 
 @Component({
   selector: 'app-columns-dialog',
-  templateUrl: './columns-dialog.component.html'
+  templateUrl: './columns-dialog.component.html',
+  animations: [Fade]
 })
 export class ColumnsDialogComponent {
+
+  hasColumnsError = false;
 
   constructor(private dialogRef: MatDialogRef<ColumnsDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public columns) {}
 
   submit(currentColumns: number[]): void {
+    this.hasColumnsError = false;
+
     let output = null;
+
+    if (currentColumns.length === 0) {
+      this.hasColumnsError = true;
+      return;
+    }
 
     const previousColumns = this.columns.selected;
     if (JSON.stringify(currentColumns) !== JSON.stringify(previousColumns)) {
