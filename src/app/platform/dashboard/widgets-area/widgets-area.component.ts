635973f7b222a37cd10bc733ffba73dd298547ee
Ruth <ruth@gigsberg.com>
2022-07-12 15:44:18 +0300

    grid

--- src/app/platform/dashboard/widgets-area/widgets-area.component.ts
+++ src/app/platform/dashboard/widgets-area/widgets-area.component.ts
@@ -1,5 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { NgxDrag, NgxResize, NgxResizeHandleType } from 'ngx-drag-resize';
+
+import { NgxResizeHandleType } from 'ngx-drag-resize';
+
 
 @Component({
   selector: 'app-widgets-area',
@@ -8,95 +10,163 @@ import { NgxDrag, NgxResize, NgxResizeHandleType } from 'ngx-drag-resize';
 })
 export class WidgetsAreaComponent implements AfterViewInit {
 
-  public style: object = {};
+  @ViewChild('gridArea') gridEl: ElementRef;
+
   readonly handleType = NgxResizeHandleType;
 
   readonly tiles = new Array(56)
 
-  @ViewChild('dragResizeArea') gridEl: ElementRef;
+  readonly TILES_IN_ROW_COUNT = 7;
+  readonly TILES_IN_COL_COUNT = 8;
 
-  elementOffsetLeft: number;
+  occupiedTiles = [];
 
   resizing = false;
-
-  readonly TILES_IN_ROW_COUNT = 8;
-  readonly TILES_IN_COL_COUNT = 7;
 
   tileWidth: number;
   tileHeight: number;
 
-  widgets = [
-    {
+  prevLocation = { offsetTop: 0, offsetLeft: 0, offsetWidth: 0, offsetHeight: 0 };
+
+  widgets = [{
       type: 'doughnut',
       left: 185.38,
       width: 358.89,
       height: 278.003,
-      top: 95
+      color: 'green',
+      position: 'absolute',
+      top: 0.6,
+      zindex: 1
     },
     {
       type: 'bar',
       left: 248.316,
       width: 433.333,
       height: 288.333,
-      top: 180
-    }
-  ];
+      color: '#bb1a1b',
+      position: 'absolute',
+      top: 0.6,
+      zindex: 1
+    }];
 
-  ngAfterViewInit() {
+  constructor() {
+    this.occupiedTiles = this.buildGrid();
+  }
+
+  ngAfterViewInit(): void {
     const dropAreaEl = this.gridEl.nativeElement;
     const width = dropAreaEl.offsetWidth;
     const height = dropAreaEl.offsetHeight;
 
-    this.tileWidth = width / this.TILES_IN_ROW_COUNT;
-    this.tileHeight = height / this.TILES_IN_COL_COUNT;
-
-    console.log(this.tileWidth);
+    this.tileWidth = width / this.TILES_IN_COL_COUNT;
+    this.tileHeight = height / this.TILES_IN_ROW_COUNT;
   }
 
-  resized(event: NgxResize, element: HTMLDivElement, index: number): void {
-    this.elementOffsetLeft = element.offsetLeft
-
-    this.resizing = true;
-    this.widgets[index].height = element.offsetHeight;
-    this.widgets[index].width = element.offsetWidth + 10;
+  buildGrid(): any {
+    return Array(this.TILES_IN_ROW_COUNT).fill(null)
+      .map(() => Array(this.TILES_IN_COL_COUNT).fill(null))
   }
 
-  dragged(event: NgxDrag, index: number): void {
+  startDrag(element: HTMLDivElement, index: number): void {
+    this.widgets[index].zindex++;
+
+    this.widgets.map((el, elIndex) => {
+      el.zindex = elIndex === index ? 2 : 1;
+      return el;
+    })
+
+    this.prevLocation.offsetLeft = element.offsetLeft;
+    this.prevLocation.offsetTop = element.offsetTop;
+    this.prevLocation.offsetWidth = element.offsetWidth;
+    this.prevLocation.offsetHeight = element.offsetHeight;
   }
 
-  startDrag(element: HTMLDivElement): void {
-    // this.elementOffsetLeft = element.offsetLeft
-    // console.log(elementRef.offsetLeft)
-  }
-
-  drag(ev, el) {
-    // console.log(el.getBoundingClientRect)
-    // ev.dataTransfer.setData("text", ev.target.id);
-  }
-
-  drop(element: HTMLDivElement, index) {
-    console.log('dropped')
+  adjust(element: HTMLDivElement, index: number) {
     if (this.resizing) {
-      this.resizing = false;
-      return;
+      this.adjustResize(element, index);
     }
 
-    this.elementOffsetLeft = element.offsetLeft
-
-    console.log((Math.floor(element.offsetLeft / 185.38)))
-    console.log(Math.floor(element.offsetTop / 95))
-    this.widgets[index].left = ((Math.floor(element.offsetLeft / 185.38)) * 185.38) - 2;
-    this.widgets[index].top = (Math.floor(element.offsetTop / 95)) * 95;
-    console.log(this.widgets[index])
-
+    this.adjustLocation(element, index);
   }
 
-  allowDrop(ev) {
-    // ev.preventDefault();
+  adjustLocation(element: HTMLDivElement | any, widgetIndex: number): void {
+    const widget = this.widgets[widgetIndex];
+    element = this.checkOverlap(element, widgetIndex) ? this.prevLocation : element;
+    console.log(this.occupiedTiles)
+    const elementValues = this.elementValues(element)
+
+    console.log(this.resizing)
+    const col = this.resizing ? elementValues.col + 1 : elementValues.col;
+    const newTop = (elementValues.row * this.tileHeight) + 5;
+    let newLeft = (col * this.tileWidth);
+    newLeft = (col * this.tileWidth) + 1;
+
+    widget.left = newLeft;
+    widget.top = newTop;
+
+    setTimeout(() => {
+      widget.top = newTop - 1;
+      widget.left = newLeft - 6;
+    })
+
+    this.resizing = false;
   }
 
-  saveCoords(elementRef) {
-    this.elementOffsetLeft = elementRef.offsetLeft
+  checkOverlap(element: HTMLDivElement, widgetIndex: number): boolean {
+    const elementValues = this.elementValues(element)
+    let overlap = false;
+
+    const tempTilesGrid = this.buildGrid();
+    for (let row = 0; row < this.TILES_IN_ROW_COUNT; row++) {
+      for (let col = 0; col < this.TILES_IN_COL_COUNT; col++) {
+        tempTilesGrid[row][col] = this.occupiedTiles[row][col];
+        const tileContent = this.occupiedTiles[row][col];
+
+        if (col >= elementValues.col && col < elementValues.col + elementValues.width && row >= elementValues.row && row < elementValues.row + elementValues.height) {
+          if (tileContent !== null && tileContent != widgetIndex) {
+            console.log(row, col)
+            overlap = true
+          }
+
+          this.occupiedTiles[row][col] = widgetIndex;
+        }
+
+        if (tileContent == widgetIndex) {
+          this.occupiedTiles[row][col] = null;
+        }
+      }
+    }
+
+    if (overlap) {
+      this.occupiedTiles = tempTilesGrid;
+    }
+
+    return overlap;
   }
 
+  //todo add model
+  elementValues(element): any {
+    return {
+      row: Math.floor(element.offsetTop / this.tileHeight),
+      col: Math.floor(element.offsetLeft / this.tileWidth),
+      width: Math.round(element.offsetWidth / this.tileWidth),
+      height: Math.round(element.offsetHeight / this.tileHeight)
+    }
+  }
+
+  adjustResize(element: HTMLDivElement, index: number): void {
+    const widget = this.widgets[index];
+    const elementValues = this.elementValues(element)
+
+    const newWidth = elementValues.width * this.tileWidth;
+    const newHeight = elementValues.height * this.tileHeight;
+
+    widget.width = newWidth;
+    widget.height = newHeight;
+
+    setTimeout(() => {
+      widget.width = newWidth - 11;
+      widget.height = newHeight - 11;
+    })
+  }
 }
--- src/app/platform/dashboard/widgets-area/widgets-area.module.ts
+++ src/app/platform/dashboard/widgets-area/widgets-area.module.ts
@@ -5,6 +5,8 @@ import { NgxDragResizeModule } from 'ngx-drag-resize';
 import { WidgetModule } from 'src/app/platform/dashboard/widgets-area/widget/widget.module';
 import { ChartModule } from 'src/app/_shared/components/chart/charts.module';
 import { MatGridListModule } from '@angular/material/grid-list';
+import {DndModule} from "ngx-drag-drop";
+import {DragDropModule} from "@angular/cdk/drag-drop";
 
 
 
@@ -15,12 +17,14 @@ import { MatGridListModule } from '@angular/material/grid-list';
   exports: [
     WidgetsAreaComponent
   ],
-    imports: [
-        CommonModule,
-        NgxDragResizeModule,
-        WidgetModule,
-        ChartModule,
-        MatGridListModule
-    ]
+  imports: [
+    CommonModule,
+    NgxDragResizeModule,
+    WidgetModule,
+    ChartModule,
+    MatGridListModule,
+    DndModule,
+    DragDropModule
+  ]
 })
 export class WidgetsAreaModule { }
