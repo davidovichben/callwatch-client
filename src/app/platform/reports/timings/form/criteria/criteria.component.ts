333004eea1013ab048630a706b1b03f1f123e9a2
Yul4 <ruthahajaj@gmail.com>
2022-10-16 17:35:59 +0300

    fixes

--- src/app/platform/reports/timings/form/criteria/criteria.component.ts
+++ src/app/platform/reports/timings/form/criteria/criteria.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
+import { AbandonTimes, ReportCriteriaModel, ReportProductionTimeRanges, ReportTimeSpaces } from 'src/app/_shared/models/report-criteria.model';
+import { ReportColumnModel } from 'src/app/_shared/models/report-column.model';
 import { ErrorMessages } from 'src/app/_shared/constants/error-messages';
 import { ReportFormats } from 'src/app/_shared/models/report-template.model';
-import { AbandonTimes, ReportCriteriaModel, ReportProductionTimeRanges, ReportTimeSpaces } from 'src/app/_shared/models/report-criteria.model';
 import { SortDirections, WeekDays } from 'src/app/_shared/constants/general';
 import { Fade } from 'src/app/_shared/constants/animations';
 
@@ -17,6 +18,7 @@ export class CriteriaComponent implements OnInit {
 
   @Input() criteria: ReportCriteriaModel;
   @Input() formGroup: FormGroup;
+  @Input() columns: ReportColumnModel[] = [];
 
   readonly errorMessages = ErrorMessages;
   readonly formats = ReportFormats;
@@ -25,8 +27,6 @@ export class CriteriaComponent implements OnInit {
   readonly abandonTimes = AbandonTimes;
   readonly sortDirections = SortDirections;
   readonly weekDays = WeekDays;
-
-  columns: object[];
 
   constructor(private fb: FormBuilder) {}
 
@@ -50,6 +50,25 @@ export class CriteriaComponent implements OnInit {
     }
   }
 
+  sortColumnSelected(value: string, index: number): void {
+    if (value) {
+      this.setColumnDisabled(value, true);
+    }
+
+    const columnId = this.formGroup.get('sort.' + index + '.column').value;
+    this.setColumnDisabled(columnId, false);
+
+    const direction = value ? 'desc' : null;
+    this.formGroup.get('sort.' + index + '.direction').patchValue(direction);
+  }
+
+  setColumnDisabled(columnId: string, disabled: boolean): void {
+    const column = this.columns.find(column => column.id === columnId);
+    if (column) {
+      column.disabled = disabled;
+    }
+  }
+
   addTime(time?: { start: string, end: string }): void {
     const group = this.fb.group({
       start: this.fb.control(time ? time.start : null),
@@ -70,9 +89,18 @@ export class CriteriaComponent implements OnInit {
     });
 
     (this.formGroup.get('sort') as FormArray).push(group);
+
+    if (sort) {
+      this.setColumnDisabled(sort.column, true)
+    }
   }
 
   removeSortColumn(index: number): void {
+    const columnId = this.formGroup.get('sort.' + index + '.column').value;
+    if (columnId) {
+      this.setColumnDisabled(columnId, false);
+    }
+
     (this.formGroup.get('sort') as FormArray).removeAt(index);
   }
 }
