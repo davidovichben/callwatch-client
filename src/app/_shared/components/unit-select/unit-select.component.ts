c18771e6eecb67663b4c7823804bf8dceec613f0
Ruth <ruth@gigsberg.com>
2023-02-23 18:22:01 +0200

    fix

--- src/app/_shared/components/unit-select/unit-select.component.ts
+++ src/app/_shared/components/unit-select/unit-select.component.ts
@@ -17,7 +17,6 @@ import { TranslatePipe } from 'src/app/_shared/pipes/translate/translate.pipe';
 
 import { Placeholder, SlideToggle } from 'src/app/_shared/constants/animations';
 import { UnitModel } from 'src/app/_shared/models/unit.model';
-import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-unit-select',
@@ -36,11 +35,6 @@ export class UnitSelectComponent implements OnInit, OnChanges, ControlValueAcces
 
   @ViewChild('dropdownEle') dropdownEle: ElementRef;
   @ViewChild('widthElement') widthElement: ElementRef;
-
-  @ViewChild('scrollViewport') scrollViewport: CdkVirtualScrollViewport;
-  private cdkVirtualScrollViewport;
-
-  @ViewChild('scrollWrapper') scrollWrapper: ElementRef;
 
   @Input() units: any[] = [];
   @Input() placeholder;
@@ -107,36 +101,6 @@ export class UnitSelectComponent implements OnInit, OnChanges, ControlValueAcces
     if (this.ignoredUnit) {
       this.ignoreUnit(this.filteredUnits);
     }
-  }
-
-  calculateContainerHeight(): string {
-    const numberOfItems = this.units.length;
-    // This should be the height of your item in pixels
-    const itemHeight = 20;
-    // The final number of items you want to keep visible
-    const visibleItems = 10;
-
-    setTimeout(() => {
-      // Makes CdkVirtualScrollViewport to refresh its internal size values after
-      // changing the container height. This should be delayed with a "setTimeout"
-      // because we want it to be executed after the container has effectively
-      // changed its height. Another option would be a resize listener for the
-      // container and call this line there but it may requires a library to detect
-      // the resize event.
-
-      this.cdkVirtualScrollViewport.checkViewportSize();
-    }, 300);
-
-    // It calculates the container height for the first items in the list
-    // It means the container will expand until it reaches `200px` (20 * 10)
-    // and will keep this size.
-    if (numberOfItems <= visibleItems) {
-      return `${itemHeight * numberOfItems}px`;
-    }
-
-    // This function is called from the template so it ensures the container will have
-    // the final height if number of items are greater than the value in "visibleItems".
-    return `${itemHeight * visibleItems}px`;
   }
 
   ignoreUnit(units: UnitModel[]): void {
